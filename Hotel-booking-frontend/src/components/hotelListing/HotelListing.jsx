9a54320aaa5aad032f1dd9a176faa0d5577b71ef
Priyammondal <priyammondal096@gmail.com>
2024-01-21 14:18:51 +0530

    push

--- Hotel-booking-frontend/src/components/hotelListing/HotelListing.jsx
+++ Hotel-booking-frontend/src/components/hotelListing/HotelListing.jsx
@@ -4,6 +4,10 @@ import HotelCard from "../hotelCard/HotelCard";
 
 const HotelListing = ({ imgData }) => {
   const hotelData = useHotelData();
+  console.log("hotelData-->",hotelData)
+  if(!Array.isArray(hotelData)){
+    return null;
+  }
   return (
     <div className="hotel-listing">
       <h3>Get away this weekend</h3>
@@ -11,7 +15,7 @@ const HotelListing = ({ imgData }) => {
         <div>Loading hotels...</div>
       ) : (
         <div className="hotel-cards">
-          {hotelData.map((hotel, index) => {
+          {hotelData?.map((hotel, index) => {
             return <HotelCard img={imgData[index]} data={hotel} key={index} />;
           })}
         </div>
